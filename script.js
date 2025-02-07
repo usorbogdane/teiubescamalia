57a02c8cf770d08f4482e5c025a348cc48f2d7be
usorbogdane <111027044+usorbogdane@users.noreply.github.com>
2025-02-07 14:44:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "esti sigura?",
+    "sigura sigura??",
+    "pe bune?",
+    "iubitaaa...",
+    "te rog gandeste te!",
+    "daca zici nu ma superiiii...",
+    "o sa fiu suparat rau...",
+    "foarte foarte foarte suparat...",
+    "bine nu te mai intreb...",
+    "glumesccc ahaha zii te rog daaaa! ❤️"
 ];
 
 let messageIndex = 0;
